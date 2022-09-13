0cc07f9221f06907e89c0c238858462f4dfd66c6
kazusukechin <kazusukechin@gmail.com>
2022-09-13 19:15:27 +0900

    機械学習の方法習得

--- mvp_2/main_mvp_app.py
+++ mvp_2/main_mvp_app.py
@@ -67,17 +67,34 @@ if uploaded_files:
                 
                 lr = LogisticRegression()
 
+                # hensu_1 = st.number_input('変数1')
+                # hensu_2 = st.number_input('変数2')
+                # hensu_3 = st.number_input('変数3')
+                # hensu_4 = st.number_input('変数4')
+                hensu_1 = 4
+                hensu_2 = 5
+                hensu_3 = 6
+                hensu_4 = 7
+                aim = st.number_input('目的関数')
+
                 # 実行ボタンを押したら下記が進む
                 if execute:
                         df_ex = df[ex]
                         df_ob = df[ob]
-                        st.text(f'df_ex:{df_ex}')
-                        st.text(f'df_ob:{df_ob}')
                         # X_train, X_test, y_train, y_test = train_test_split(df_ex.values, df_ob.values, test_size = 0.3)
-                        x_train = df_ex.values
+                        X_train = df_ex.values
                         y_train = df_ob.values
+                        X_test = pd.DataFrame([], columns=['変数1', '変数2', '変数3', '変数4'])
+                        record = pd.Series([hensu_1, hensu_2, hensu_3, hensu_4], index=X_test.columns)
+                        X_test = X_test.append(record, ignore_index=True)
+
+                        
 
                         lr.fit(X_train, y_train)
+                        pred_probs = lr.predict(X_test)
+                        st.write(pred_probs)
+                        y_test = [pred_probs[0]]
+
                         # プログレスバー（ここでは、やってる感だけ）
                         my_bar = st.progress(0)
                         for percent_complete in range(100):
@@ -87,3 +104,6 @@ if uploaded_files:
                         col1, col2 = st.columns(2)
                         col1.metric(label="トレーニングスコア", value=lr.score(X_train, y_train))
                         col2.metric(label="テストスコア", value=lr.score(X_test, y_test))
+
+                        ### 結果出力
+                        st.write(pred_probs[0])
